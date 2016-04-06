692bab3059450d480cbb5ab62f07151c6122352a
zh99998 <zh99998@gmail.com>
2016-04-06 06:20:45 +0000

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@
 echo "machine github.com login ${GITHUB_USERNAME} password ${GITHUB_PASSWORD}" > ~/.netrc
 chmod 600 ~/.netrc
 
-xvfb-run --server-args="-screen 0 1x0x24" ruby Server.rb
+xvfb-run --server-args="-screen 0 1x0x24" ./Server.rb
