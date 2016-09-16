a5814b58347ebb98a75ee2a5fba67a1f836b85f4
不如归去 <IamIpanda@users.noreply.github.com>
2016-09-16 23:07:06 +0800

    Update Log.rb
    switch Logger to STDERR

--- Log.rb
+++ Log.rb
@@ -4,6 +4,8 @@ require 'logger'
 module Log
 	class << self
 		attr_accessor :logger
+		attr_accessor :out
 	end
-	self.logger = Logger.new STDOUT
+	self.logger = Logger.new STDERR
+	self.out = Logger.new STDOUR
 end
